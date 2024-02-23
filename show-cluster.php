3c641b005a752178c4dae54aa3a0a410dc97e8bd
KViolinov <152738737+KViolinov@users.noreply.github.com>
2024-02-23 17:28:25 +0200

    Update show-cluster.php

--- show-cluster.php
+++ show-cluster.php
@@ -140,10 +140,10 @@ session_start();
             </thread>
             <tbody>
                 <?php
-                $servername = "localhost";
-                $username = "kvbbgcom_floodPrevention";
-                $password = "kv0889909595";
-                $database = "kvbbgcom_floodPrevention";
+                $servername = "***";
+                $username = "***";
+                $password = "***";
+                $database = "***";
                 
                 $connection = new mysqli($servername, $username, $password, $database);
                 
