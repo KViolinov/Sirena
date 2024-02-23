b4457e8c40558166f616e2fb3d24874c1db4c6e9
KViolinov <152738737+KViolinov@users.noreply.github.com>
2024-02-23 11:30:26 +0200

    Create zanas.php

---
+++ zanas.php
@@ -0,0 +1,314 @@
+<!-- za nas -->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>СИРЕНА - За нас</title>
+        <meta name="description" content="СИРЕНА - Система за Известяване и Ранна Евакуация при Наводнения и Аварии">
+        <meta name="author" content="Konstantin Violinov, Константин Виолинов">
+        <meta name="area" content="Education, Smart Home, IoT, Internet of Thing, Arduino, Образование, Умен дом, Интернет на нещата, Ардуино, Обучение" />
+        <meta name="rating" content="General" />
+        <meta name="abstract" content="СИРЕНА - Система за Известяване и Ранна Евакуация при Наводнения и Аварии. Целта на този сайт е да представи работеща платформа за ранно известяване при опасност от наводнения." />
+        <meta name="keywords" content="Обучение, Образование, Умен дом, Промишлена автоматизация, Проекти, Проекти с Ардуино, Проекти за умен дом, Проекти за примишлена автоматизация, Проекти за IoT, 
+       Education, smart home for beginners, smart home for advanced, Arduino, smart home with arduino, Projects with Arduino, ESP32, LoRa32,
+       Making Smart Home, Building Smart Home, Internet of Thing, IoT, Би смарт, B-Smart, Be Smart, B Smart " />
+        <meta name="subject" content="СИРЕНА - Система за Известяване и Ранна Евакуация при Наводнения и Аварии, промишлена автоматизация, SIRENA, Internet of Thing, IoT, Projects" />
+       	<meta name="robots" content="index, follow" />
+        <!-- 
+        Multi Profile
+        http://www.templatemo.com/preview/templatemo_457_multi_profile
+        -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700' rel='stylesheet' type='text/css'>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/font-awesome.min.css" rel="stylesheet">
+        <link href="css/templatemo-style.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+        
+        <style>
+        
+        .row{
+                display:flex;
+                 justify-content: space-around;
+                align-items: center;
+                
+            }
+        
+            .zanas{
+                display:flex;
+                 justify-content: space-between;
+                align-items: center;
+                flex-direction: column;
+            }
+            .pic{
+                display:flex;
+                justify-content: space-between;
+                align-items: center;
+            }
+            
+            .text{
+                text-align: center;
+            }
+            
+           .text-uppercase{
+               text-align: left;
+           }
+           
+           
+        </style>
+        
+
+    </head>
+    <body class="templatemo-container">
+        <!-- header -->
+        <div class="header-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-4 col-sm-4 site-name-container">
+                        <img src="img/logo.png" alt="Site logo" class="site-logo">
+                        <h1 class="site-name">СИРЕНА</h1>
+                    </div>
+                    <div class="mobile-menu-icon">
+                        <i class="fa fa-bars"></i>
+                    </div>
+                    <div class="col-lg-9 col-md-8 col-sm-8 templatemo-nav-container">
+                        <nav class="templatemo-nav">
+                            <ul>
+                                <li><a href="index.php">Начало</a></li>
+                                <li><a href="visualization.php">Визуализация</a></li>
+                                <li><a href="show-cluster.php">Клъстери</a></li>
+                                <li><a href="show-devices.php">Устройства</a></li>
+                                <li><a href="zanas.php" class="active">За нас</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end header -->
+        <!-- header image -->
+       
+        </div>
+        
+        
+        <section class="templatemo-container light-gray-bg section-shadow-bottom">
+            <div class="container">
+                <div class="row section-title-container">
+                    <div class="col-lg-12 text-uppercase text-center">
+                        <h2 class="section-title">Кои сме ние</h2>
+                        <div class="section-title-underline-blue"></div>
+                        <hr class="section-title-underline">
+                    </div>
+                </div> <!-- row -->
+                <div class="row">
+                    <!--<div class="col-lg-4 col-md-4 col-sm-4 text-center tm-about-container-2">-->
+                    <div class="zanas">
+                        <img src="img/kosi2.jpg" class="pic" alt="Image">
+                        <div class="text">
+                        <h3 class="text-uppercase about-h3">Константин Виолинов</h3>
+                        <p class="about-description">9 г клас</p>
+                        <p class="about-description">Системен програмист</p>
+                        <p class="about-description">ПМГ "Васил Друмев"</p>
+                        <p class="about-description">Велико Търново</p>
+                        </div>
+                    </div>
+                    <div class="zanas">
+                        <img src="img/ignatov2.jpg" class="pic" alt="Image">
+                        <div class="text">
+                        <h3 class="text-uppercase about-h3">Георги Игнатов</h3>
+                        <p class="about-description">старши учител</p>
+                        <p class="about-description">ПМГ "Васил Друмев"</p>
+                        <p class="about-description">Велико Търново</p><br>
+                        <p class="about-description"></p>
+                        </div>
+                    </div>
+                   
+                   
+                 
+                </div>
+            </div>
+        </section>
+        <div class="welcome-container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="welcome-wrapper">
+                            <h2 class="welcome-title text-uppercase">За връзка</h2>
+                            <img src="img/welcome-divider-lines.png" alt="Welcome divider" class="welcome-divider-lines-img">
+                            <p class="welcome-description">Начин да се свържете с нас</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Welcome message -->
+        <!-- main content -->
+        <section class="templatemo-container">
+            <div class="container">
+                <div class="row margin-bottom-30">
+                    <div class="col-lg-12 text-uppercase text-center">
+                        <h2 class="section-title">Контактна форма</h2>
+                        <div class="section-title-underline-blue"></div>
+                        <hr class="section-title-underline">
+                    </div> <!-- col-lg-12 -->
+                    
+                </div> <!-- row -->
+                <div class="row margin-bottom-30">
+                    <div class="col-lg-6 col-md-6 contact-left">
+                        <h3 class="section-title-2 text-uppercase font-weight-300"><b>Свържете</b> <span class="blue-text">се с нас</span></h3>
+                        <div class="contact-point">
+                            <i class="fa fa-map-marker fa-3x contact-point-icon blue-text"></i>
+                            <div class="contact-point-detail-container">
+                                <b class="text-uppercase contact-point-title blue-text font-weight-700">Адрес</b>
+                                <p class="contact-point-detail">гр. Велико Търново, ул. "Вела Благоева" №10</p>
+                            </div>
+                        </div>
+                        <div class="contact-point">
+                            <i class="fa fa-envelope-o fa-3x contact-point-icon blue-text"></i>
+                            <div class="contact-point-detail-container">
+                                <b class="text-uppercase contact-point-title blue-text font-weight-700">Електронна поща</b>
+                                <p class="contact-point-detail">info-300122@edu.mon.bg</p>
+                            </div>
+                        </div>
+                        <div class="contact-point">
+                            <i class="fa fa-phone fa-3x contact-point-icon blue-text"></i>
+                            <div class="contact-point-detail-container">
+                                <b class="text-uppercase contact-point-title blue-text font-weight-700">Телефон</b>
+                                <p class="contact-point-detail">062/ 629 506</p>
+                            </div>
+                       </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 contact-right">
+                        <h3 class="section-title-2 text-uppercase font-weight-300"><b>Оставете</b> <span class="blue-text">съобщение</span></h3>
+                        <form action="send_mail.php" method="POST" class="tm-contact-form" enctype="multipart/form-data">
+                            <div class="tm-contact-form">
+                                <div class="form-group">
+                                    <input type="text" id="contact_name" name="contact_name" class="form-control" placeholder="Вашето име" required/>
+                                </div>
+                                <div class="form-group">
+                                    <input type="email" id="contact_email" name="contact_email" class="form-control" placeholder="Електронна поща за обратна връзка" required/>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" id="contact_subject" name="contact_subject" class="form-control" placeholder="Тема" required/>
+                                </div>
+                                <div class="form-group">
+                                    <textarea id="contact_message" name="contact_message" class="form-control" rows="8" placeholder="Съобщение" required></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" name="submit" class="text-uppercase btn-blue-gradient btn-submit">Изпрати съобщението</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div> <!-- row -->
+                
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2914.2998804751987!2d25.619916875668135!3d43.077190389495506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40a9260deb576cfd%3A0x55507c06084147ae!2sPrirodomatematicheska%20Gimnaziya%20Vasil%20Drumev!5e0!3m2!1sen!2sbg!4v1699259419302!5m2!1sen!2sbg" width="100%" height="550" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div> <!-- container -->
+        </section>
+        
+        <div class="blue-divider effect1">
+            <div class="dark-blue-bg"></div>
+            <div class="blue-divider-bg-graphic"></div>
+            <div class="blue-bg"></div>
+        </div>
+        
+        
+        <section class="templatemo-container blue-bg footer-nav effect1">
+            <div class="container">
+         
+                <div class="col-lg-2 col-md-2 col-sm-6 footer-block">
+                    <h3 class="text-uppercase">Основно меню</h3>
+                    <nav class="text-uppercase templatemo-nav-2">
+                        <ul>
+                            <li><a href="index.php" class="active">Начало</a></li>
+                                <li><a href="visualization.php">Визуализация</a></li>
+                                <li><a href="show-cluster.php">Клъстери</a></li>
+                                <li><a href="show-devices.php">Устройства</a></li>
+                                <li><a href="zanas.php">За нас</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 footer-block">
+                    <h3 class="text-uppercase">За нас</h3>
+                    <p>Настоящият сайт е разработен като част от демонстрационна платформа СИРЕНА - "Система за Известяване и Ранна Евакуация при Наводнения и Аварии"
+                        за участие в 21-ва Национална олимпиада по информационни технологии 2023/24
+                    </p>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 footer-block">
+                    <h3 class="text-uppercase">Ръководител</h3>
+                    <ul>
+                        <li><a href="#">Георги Игнатов</a></li>
+                        <li><a href="#">старши учител<a></li>
+                        <li><a href="#">ПМГ "Васил Друмев"</a></li>
+                        <li><a href="#">Велико Търново</a></li>
+                        <li><a href="http://www.pmgvt.org" target="_blank">www.pmgvt.org</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 footer-block">
+                    <h3 class="text-uppercase">Автор</h3>
+                    <ul>
+                        <li><a href="#">Константин Виолинов</a></li>
+                        <li><a href="#">ПМГ "Васил Друмев"</a></li>
+                        <li><a href="#">Велико Търново</a></li>
+                        <li><a href="http://www.pmgvt.org" target="_blank">www.pmgvt.org</a></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <!-- end main content -->
+        <footer class="text-center">
+            <p class="small copyright-text">Copyright &copy; 2024 Константин Виолинов| Design: Template Mo</p>
+        </footer>
+
+        <!-- JS -->
+        <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>      <!-- jQuery -->
+        <script type="text/javascript" src="js/templatemo-script.js"></script>      <!-- Templatemo Script -->
+        <script>
+            /* Google map
+            ------------------------------------------------*/
+            /*
+            var map = '';
+            var center;
+
+            function initialize() {
+                var mapOptions = {
+                  zoom: 16,
+                  center: new google.maps.LatLng(43.07733536843857, 25.62251325463919),
+                  scrollwheel: false
+                };
+
+                map = new google.maps.Map(document.getElementById('google-map'),  mapOptions);
+
+                google.maps.event.addDomListener(map, 'idle', function() {
+                    calculateCenter();
+                });
+
+                google.maps.event.addDomListener(window, 'resize', function() {
+                    map.setCenter(center);
+                });
+            }
+
+            function calculateCenter() {
+              center = map.getCenter();
+            }
+
+            function loadGoogleMap(){
+                var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+                document.body.appendChild(script);
+            }
+            $(document).ready(function(){
+                loadGoogleMap();
+            }); */
+        </script>
+    </body>
+</html>
