00ee8989a8def8e190d1e5913b39523c8fd62941
KViolinov <152738737+KViolinov@users.noreply.github.com>
2024-02-24 11:49:13 +0200

    Delete map directory

--- map/js/googlemap.js
+++
@@ -1,7 +0,0 @@
-function loadMap() {
-    var pune = {lat: 18.5204, lng: 73.8567};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 12,
-      center: pune
-    });
-}
